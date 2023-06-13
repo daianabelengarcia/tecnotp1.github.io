2e1978c990fdd8aebb36aead40a891ff29d6bd60
Daiana Belen Garcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-06-13 12:13:01 -0300

    nosenose

--- sketch.js
+++ sketch.js
@@ -35,8 +35,8 @@ function setup() {
   mic.start();
 
   userStartAudio(); // esto lo utilizo porque en algunos navigadores se cuelga el audio. Esto hace un reset del motor de audio (audio context)
-  
-  for (let i=0; i<10; i++) {
+
+  for (let i = 0; i < 10; i++) {
     grafico[i] = createGraphics(width, height);
   }
 
@@ -53,11 +53,11 @@ function setup() {
 
 }
 
-function preload(){
-  for (let i=0; i<2; i++){
-    imagen[i] = loadImage('img/prueba-capa-'+i+'.jpg');
+function preload() {
+  for (let i = 0; i < 2; i++) {
+    imagen[i] = loadImage('img/prueba-capa-' + i + '.jpg');
   }
-  
+
 }
 
 function draw() {
@@ -65,82 +65,88 @@ function draw() {
   amp = mic.getLevel();
 
   haySonido = amp > AMP_MIN;
-  volumenBajo = amp < 0.15;
-  let subioelVolumen = haySonido && !volumenAlto;
+  volumenBajo = amp < 0.15; //el volumen es bajo mientras no supere esa amplitud
+  let subioelVolumen = haySonido && volumenAlto; //sube el volumen si hay sonido y el volumen es alto
 
-  if (volumenBajo) {
-    eligeImagen = 0;
-  } else if (subioelVolumen){
-    eligeImagen = 1;
-  }
+
 
   if (capa == 0) {
+    if (volumenBajo) { //elige la imagen que se muestra en la mascara de recorte dependiendo el volumen del sonido
+      eligeImagen = 0;
+    } else if (subioelVolumen) {
+      eligeImagen = 1;
+    }
+
     for (let i = 0; i < 20; i++) {
       if (haySonido) {
-          p[i].dibujarGrafico(grafico[0]);
-        } 
+        p[i].dibujarGrafico(grafico[0]);
       }
     }
-  
-    let copia = imagen[eligeImagen].get();
-    copia.mask(grafico[0]); 
-   image(copia, 0, 0, width, height); 
 
-   if (p[0].posY >= height) {
+    let copia = imagen[eligeImagen].get();
+    copia.mask(grafico[0]);
+    image(copia, 0, 0, width, height);
+  }
+
+
+
+  if (p[0].posY >= height) {
     capa = 1;
   }
+
+
   if (capa == 1) {
     for (let i = 0; i < 20; i++) {
       if (haySonido) {
-          p2[i].dibujarGrafico2(grafico[1]);
-        }
+        p2[i].dibujarGrafico2(grafico[1]);
       }
     }
-  
+  }
+
   copia = imagen[eligeImagen].get();
   copia.mask(grafico[1]);
-  image(copia, 0, 0, width, height); 
+  image(copia, 0, 0, width, height);
 
-  volumenAlto = !volumenBajo;
+  volumenAlto = !volumenBajo; //volumen alto es verdadero cuando la otra condicion deja de serlo
 
- /* if (p2[0].posY >= height) {
-    capa = 2;
-  }
-  if (capa == 2) {
-    for (let i = 0; i < 30; i++) {
-      if (haySonido) {
-        if (i % 2 == 0) {
-          p3[i].dibujar(amp, fondo);
-        } else {
-          p3[i].dibujar2(amp, fondo);
-        }
-      }
-    }
-  }
-  if (p3[0].posY >= height) {
-    capa = 3;
-  }
-  if (capa == 3) {
-    for (let i = 0; i < 30; i++) {
-      if (haySonido) {
-        if (i % 2 == 0) {
-          p4[i].dibujar(amp, fondo);
-        } else {
-          p4[i].dibujar2(amp, fondo);
-        }
-      }
-    }
-  }
-  if (p4[0].posY >= height) {
-    capa = 4;
-  }
-  
-  if (capa == 4) {
-    cuadrados.dibujar(haySonido);
-  }
-  if (IMPRIMIR) {
-    imprimirData();
-  } */
+  /* if (p2[0].posY >= height) {
+     capa = 2;
+   }
+   if (capa == 2) {
+     for (let i = 0; i < 30; i++) {
+       if (haySonido) {
+         if (i % 2 == 0) {
+           p3[i].dibujar(amp, fondo);
+         } else {
+           p3[i].dibujar2(amp, fondo);
+         }
+       }
+     }
+   }
+   if (p3[0].posY >= height) {
+     capa = 3;
+   }
+   if (capa == 3) {
+     for (let i = 0; i < 30; i++) {
+       if (haySonido) {
+         if (i % 2 == 0) {
+           p4[i].dibujar(amp, fondo);
+         } else {
+           p4[i].dibujar2(amp, fondo);
+         }
+       }
+     }
+   }
+   if (p4[0].posY >= height) {
+     capa = 4;
+   }
+   
+   if (capa == 4) {
+     cuadrados.dibujar(haySonido);
+   }
+   if (IMPRIMIR) {
+     imprimirData();
+   } */
 }
 
 function imprimirData() {
