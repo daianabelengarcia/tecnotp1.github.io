e49330a518c4cbd52603652188a70320ca04b58c
Daiana Belen Garcia <103230866+daianabelengarcia@users.noreply.github.com>
2023-06-14 14:37:44 -0300

    velocidad

--- js/pincelada.js
+++ js/pincelada.js
@@ -11,19 +11,19 @@ class Pincelada {
 
   dibujarGrafico(grafico) {
 
-    this.posY = this.y += 2;
+    this.posY = this.y += 5;
     this.x = sin(this.counter) * 20 + this.posX;
 
     grafico.fill(0);
     grafico.noStroke();
     grafico.ellipse(this.x, this.posY, this.tam, this.tam);
 
-    this.counter += 0.05;
+    this.counter += random(0.03,0.05);
   }
 
   dibujarGrafico2(grafico) {
 
-    this.posY = this.y += 2;
+    this.posY = this.y += 5;
     this.x = sin(this.counter) * 20 + this.posX;
 
     grafico.fill(0);
